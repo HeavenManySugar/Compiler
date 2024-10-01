e5c5716e8c55bde06461e0ce4dd23b0aab88dfc2
ZRE <chy853@gmail.com>
2024-10-01 20:28:20 +0800

    Add code for exercise6 in x86-64 Assembly

---
+++ HW1 x86-64 Assembly/exercise6.s
@@ -0,0 +1,42 @@
+.text
+    .globl main
+
+isqrt:
+        xorq    %rax, %rax      
+        movq    $1, %rcx        
+        jmp     T1
+L1:     incq    %rax
+        leaq    1(%rcx, %rax, 2), %rcx
+T1:     cmpq    %rdi, %rcx      
+        jle     L1
+        ret
+
+main:
+    .local n
+    .equ n, -8
+    push %rbp
+    movq %rsp, %rbp
+    sub $32, %rsp
+    movq $0, n(%rbp)        
+loop:
+    movq n(%rbp), %rdi
+    call isqrt
+    # print
+    movq $format, %rdi
+    movq n(%rbp), %rsi
+    movq %rax, %rdx
+    movq $0, %rax
+    call printf
+
+    incq n(%rbp)
+    cmpq $20, n(%rbp)
+    jle loop
+
+    xor %rax, %rax
+    movq %rbp, %rsp
+    pop %rbp
+    ret
+
+.data
+format:
+    .string "sqrt(%2d) = %2d\n"
